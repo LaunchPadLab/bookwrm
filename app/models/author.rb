4ab5c4d65a78acf50709a47a7e065539ef1af31f
Rachel Killackey <rachel@launchpadlab.com>
2018-09-18 09:21:16 -0500

    use first_name and last_name for authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
   # has_many :books, dependent: :destroy
 
-  validates_presence_of :name
+  validates_presence_of :first_name, :last_name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema.define(version: 2018_09_17_151154) do
   enable_extension "plpgsql"
 
   create_table "authors", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
