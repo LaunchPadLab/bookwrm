be9e217131cd7ea1467a55a07eb190a371084b6a
Ifat <iribon9@gmail.com>
2018-10-16 14:57:49 -0500

    dont need update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    redirect_to books_path unless current_user == @user
   end
 
   # GET /users/new
